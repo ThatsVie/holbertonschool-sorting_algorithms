ebbb2b607f6257feb4abb266faa04714afe74f4b
ThatsVie <Viepaulalmt@gmail.com>
2023-12-11 13:13:16 -0800

    starting quick clear

---
+++ 3-quick_sort.c
@@ -0,0 +1,8 @@
+#include "sort.h"
+/**
+ * quick_sort - sorts an array of integers in ascending order
+ * using the Quick sort algorithm
+ * @array:
+ * @size:
+ */
+void quick_sort(int *array, size_t size);

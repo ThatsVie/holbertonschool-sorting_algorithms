33e525b1e4f8170e8cac8204f880789017c5f83f
ThatsVie <Viepaulalmt@gmail.com>
2023-12-13 11:24:13 -0800

    additional quicksort function prototypes added to header

--- sort.h
+++ sort.h
@@ -23,6 +23,9 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void perform_swap(int *element1, int *element2);
+void swap_elements(int *array, int index1, int index2, size_t size);
+int partition(int *array, int start, int end, size_t size);
+void recursive_quicksort(int *array, int start, int end, size_t size);
 void quick_sort(int *array, size_t size);
 
 #endif
